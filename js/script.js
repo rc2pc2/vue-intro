9ca9b7ed527e7d71985bb232c20e2f625c7d631b
rc2pc2 <petriccarcc@gmail.com>
2023-06-05 09:45:09 +0200

    basic usage example of v-bind in vue

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 const { createApp } = Vue;
+
 createApp({
     data() {
         return {
             message: 'Hello Vue!',
-            classes : 'text-white bg-dark p-5 text-center'
+            classes : 'text-white bg-dark p-5 text-center',
+            imageSrc : './img/paperella.webp',
         }
     },
 
